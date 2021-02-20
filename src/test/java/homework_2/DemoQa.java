e0b09a6865acb75b339013419d528524c255aa38
Sfairat00 <s.ojereliew@yandex.ru>
2021-02-20 18:23:54 +0300

    добавил шаги

--- src/test/java/homework_2/DemoQa.java
+++ src/test/java/homework_2/DemoQa.java
@@ -14,6 +14,14 @@ public class DemoQa {
         $("#lastName").setValue("FFFF");
         $("#userEmail").setValue("sfai@qq.ru");
         $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("9161764617");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("1986");
+        $(".react-datepicker__day.react-datepicker__day--019").click();
+        $("#subjectsInput").setValue("BYXAT").pressEnter();
+
+        sleep(4000);
 
     }
 
